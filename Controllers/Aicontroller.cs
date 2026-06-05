dc68a6ae0efd5d7735bd4162f43c59f8b33c5edf
pavel svoboda <pavel97svoboda@seznam.cz>
2026-06-05 23:52:05 +0200

    I'm going to sleep

---
+++ Controllers/Aicontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogicCRM.Controllers
+{
+    public class AIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
